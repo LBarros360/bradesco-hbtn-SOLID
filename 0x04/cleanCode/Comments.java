e28608354e6a198031554d3ebd724080441a5a18
LBarros360 <lbarros360@gmail.com>
2025-11-08 20:48:36 -0300

    Create Comments.java

---
+++ 0x04/cleanCode/Comments.java
@@ -0,0 +1,21 @@
+public class Comments {
+
+    public Owner getTestcaseOwner(Testcase testcase){};
+
+    public Hours getHoursInQuarter(){};
+
+    /*
+     * Esta classe avalia a visão geral dos casos de teste que são cancelados pelo cliente
+     */
+
+    /**
+     * <p> este método publica resultados com base na data
+     * </p>
+     * @param data data a partir da qual os resultados devem ser publicados
+     * @retornar Lista de entidades de resultado
+     * @desde 16.0
+     */
+    public List<Result> publishResults(Date date) {
+        // alguma lógica de negócios aqui
+    }
+}
